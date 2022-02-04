3b680306c3fce4a7d3f9f4f1145ef91694f2223c
macenuw <gnykabiny54@gamil.com>
2022-02-04 20:49:37 +0200

    done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,32 @@
-$(function(){
-
+$(function () {
+  $("#forMonster").on("click", function () {
+    $(".tab__title").removeClass("active");
+    $(this).toggleClass("active");
+    $(".catalog__info-inner").removeClass("active");
+    $('#monster').addClass('active')
+  })
+  $("#forBank").on("click", function () {
+    $(".tab__title").removeClass("active");
+    $(this).toggleClass("active");
+    $(".catalog__info-inner").removeClass("active");
+    $('#bank').addClass('active')
+  })
+  $("#forPack").on("click", function () {
+    $(".tab__title").removeClass("active");
+    $(this).toggleClass("active");
+    $(".catalog__info-inner").removeClass("active");
+    $('#pack').addClass('active')
+  })
+  $("#forOffer").on("click", function () {
+    $(".tab__title").removeClass("active");
+    $(this).toggleClass("active");
+    $(".catalog__info-inner").removeClass("active");
+    $('#specialOffer').addClass('active')
+  })
+  $("#forPromo").on("click", function () {
+    $(".tab__title").removeClass("active");
+    $(this).toggleClass("active");
+    $(".catalog__info-inner").removeClass("active");
+    $('#promo').addClass('active')
+  })
 })
